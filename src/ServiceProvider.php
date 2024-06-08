7d0dd1964a1087cc0e7b628d5b179cfc2fe67fd7
Andrey Helldar <helldar@dragon-code.pro>
2024-06-08 23:47:32 +0300

    Setup service provider and composer settings

---
+++ src/ServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelLang\Routes;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+}
