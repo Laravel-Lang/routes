ec095e0e6a57357bf58ddb4b46453c2e5bd8e602
Andrey Helldar <helldar@dragon-code.pro>
2024-06-09 13:29:56 +0300

    Improved tests for sessions

--- tests/Feature/SessionTest.php
+++ tests/Feature/SessionTest.php
@@ -8,22 +8,31 @@ use Tests\Constants\LocaleValue;
 use function Pest\Laravel\withSession;
 
 test('main locale', function (string $locale) {
+    $time = time();
+
     withSession([Config::shared()->routes->names->session => $locale])
-        ->getJson(route('via.session'))
+        ->getJson(route('via.session', compact('time')))
         ->assertSuccessful()
-        ->assertJsonPath('message', LocaleValue::TranslationFrench);
+        ->assertJsonPath('message', LocaleValue::TranslationFrench)
+        ->assertJsonPath('time', $time);
 })->with('main-locales');
 
 test('aliased locale', function (string $locale) {
+    $time = time();
+
     withSession([Config::shared()->routes->names->session => $locale])
-        ->getJson(route('via.session'))
+        ->getJson(route('via.session', compact('time')))
         ->assertSuccessful()
-        ->assertJsonPath('message', LocaleValue::TranslationGerman);
+        ->assertJsonPath('message', LocaleValue::TranslationGerman)
+        ->assertJsonPath('time', $time);
 })->with('aliased-locales');
 
 test('empty locale', function (int|string|null $locale) {
+    $time = time();
+
     withSession([Config::shared()->routes->names->session => $locale])
-        ->getJson(route('via.session'))
+        ->getJson(route('via.session', compact('time')))
         ->assertSuccessful()
-        ->assertJsonPath('message', LocaleValue::TranslationFrench);
+        ->assertJsonPath('message', LocaleValue::TranslationFrench)
+        ->assertJsonPath('time', $time);
 })->with('empty-locales');
