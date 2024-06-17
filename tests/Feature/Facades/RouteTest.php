31b3fd8c03a0389a012f96a840d8f4d325ffa326
Andrey Helldar <helldar@dragon-code.pro>
2024-06-17 11:10:21 +0300

    Added `localizedGroup` macro for `Route` facade

--- tests/Feature/Facades/RouteTest.php
+++ tests/Feature/Facades/RouteTest.php
@@ -5,15 +5,28 @@ declare(strict_types=1);
 use LaravelLang\Routes\Helpers\Route as RouteName;
 use Tests\Constants\LocaleValue;
 
-test('group', function () {
+test('group: facade', function () {
     $foo = 'test';
 
-    expect(route('via.group', compact('foo')))
+    expect(route('via.group.facade', compact('foo')))
         ->toBeString()
-        ->toBe('http://localhost/group/test');
+        ->toBe('http://localhost/group/facade/test');
 
-    expect(route(RouteName::prefix() . 'via.group', [
+    expect(route(RouteName::prefix() . 'via.group.facade', [
         'locale' => LocaleValue::LocaleMain,
         'foo'    => $foo,
-    ]))->toBeString()->toBe('http://localhost/fr/group/test');
+    ]))->toBeString()->toBe('http://localhost/fr/group/facade/test');
+});
+
+test('group: macro', function () {
+    $foo = 'test';
+
+    expect(route('via.group.macro', compact('foo')))
+        ->toBeString()
+        ->toBe('http://localhost/group/macro/test');
+
+    expect(route(RouteName::prefix() . 'via.group.macro', [
+        'locale' => LocaleValue::LocaleMain,
+        'foo'    => $foo,
+    ]))->toBeString()->toBe('http://localhost/fr/group/macro/test');
 });
