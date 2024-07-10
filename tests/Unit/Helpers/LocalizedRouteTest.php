644c389e2b67b906bd1ccd12ab95a975069eacd2
Andrey Helldar <helldar@dragon-code.pro>
2024-07-10 13:27:35 +0300

    Added `localizedRoute` function

---
+++ tests/Unit/Helpers/LocalizedRouteTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use LaravelLang\Config\Facades\Config;
+use Tests\Constants\LocaleValue;
+
+test('route groups', function () {
+    $name     = Config::shared()->routes->namePrefix;
+    $locale   = LocaleValue::LocaleMain;
+    $fallback = LocaleValue::LocaleAliasParent;
+
+    expect(localizedRoute($name . 'via.group.facade', ['foo' => 'bar']))
+        ->toEndWith("localhost/$locale/group/facade/bar");
+
+    expect(localizedRoute($name . 'via.group.macro', ['foo' => 'bar', 'locale' => $fallback]))
+        ->toEndWith("localhost/$fallback/group/macro/bar");
+
+    expect(localizedRoute('via.group.facade', ['foo' => 'bar']))
+        ->toEndWith("localhost/$locale/group/facade/bar");
+
+    expect(localizedRoute('via.group.macro', ['foo' => 'bar', 'locale' => $fallback]))
+        ->toEndWith("localhost/$fallback/group/macro/bar");
+});
+
+test('routes without groups', function () {
+    expect(localizedRoute('via.model.default', ['foo' => 'bar']))
+        ->toEndWith('localhost/model/default/bar');
+});
