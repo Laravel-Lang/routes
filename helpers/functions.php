405486aec2db85258d6d7e8a1afecd4bae00c431
Andrey Helldar <helldar@dragon-code.pro>
2025-04-14 16:18:38 +0300

    Update functions.php

--- helpers/functions.php
+++ helpers/functions.php
@@ -8,24 +8,22 @@ use LaravelLang\Config\Facades\Config;
 use LaravelLang\Routes\Helpers\Route as RouteHelper;
 
 if (! function_exists('localizedRoute')) {
-    function localizedRoute(
-        string $route,
-        array $parameters = [],
-        bool $absolute = true
-    ): string {
-        $localeKey = Config::shared()->routes->names->parameter;
+    function localizedRoute(string $route, array $parameters = [], bool $absolute = true): string
+    {
+        $locale = Config::shared()->routes->names->parameter;
+        $prefix = Config::shared()->routes->namePrefix;
 
         if (RouteHelper::hidingFallback()) {
-            unset($parameters[$localeKey]);
+            unset($parameters[$locale]);
+
             return route($route, $parameters, $absolute);
         }
 
-        $prefix = Config::shared()->routes->namePrefix;
-        $name = Str::startsWith($route, $prefix) ? $route : Str::start($route, $prefix);
+        $name = Str::start($route, $prefix);
         $route = Route::has($name) ? $name : $route;
 
         return route($route, array_merge([
-            $localeKey => app()->getLocale(),
+            $locale => app()->getLocale(),
         ], $parameters), $absolute);
     }
 }
