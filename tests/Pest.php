aca1132e4fd6a20ec849e83b232affa953636dae
Andrey Helldar <helldar@dragon-code.pro>
2025-10-09 20:09:15 +0300

    Reordered `->in()` method in Pest tests for better readability.

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Event;
 
 uses(Tests\TestCase::class, DatabaseTransactions::class)
-    ->in('Feature', 'Unit')
     ->beforeEach(function () {
         Event::fake();
 
         $this->withHeader('Accept-Language', 'zxc');
-    });
+    })
+    ->in('Feature', 'Unit');
